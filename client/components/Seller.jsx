cb5031d9495b00269af50dabb0c662524ffe467c
Eric Kim <kimericbyung@gmail.com>
2021-01-30 02:57:52 -0800

    Last

--- client/components/Seller.jsx
+++ client/components/Seller.jsx
@@ -120,7 +120,7 @@ class Seller extends React.Component {
             </div>
             <button type="button" className="message-close-button" onClick={this.modalOnClick}>
               <span className="etsy-message-icon-button">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="11 0 1.8 1.8" aria-hidden="true" focusable="false">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="-12 -12 48 48" aria-hidden="true" focusable="false" style={{ width: '48px' }}>
                   <path
                     d="M13.414,12l6.293-6.293a1,1,0,0,0-1.414-1.414L12,10.586,5.707,4.293A1,1,0,0,0,4.293,5.707L10.586,12,4.293,18.293a1,1,0,1,0,1.414,1.414L12,13.414l6.293,6.293a1,1,0,0,0,1.414-1.414Z"
                   />
